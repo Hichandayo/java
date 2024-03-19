2c491a8928b1d9250aa689738114cd04f7aa6757
Hichandayo <nguyenhienthe1120@gmail.com>
2024-03-19 08:58:35 +0700

    baitapjava 18-3

---
+++ baitapjava.js
@@ -0,0 +1,5 @@
+let n1 = parseInt(prompt("Nhập mức lương của bạn"));
+n2 = parseInt(prompt("Nhập mức lương của bạn"));
+const result = "";
+result = n1 > n2 ? "person1" : "person2"
+console.log(result);
